15429e0d23f2342a8c96e97cd735e22ffe8173de
Amal Thomas <52843816+AmalThomas1@users.noreply.github.com>
2023-12-15 12:04:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ def addition(a,b,c):
 def multiplication(a,c,d):
     return a*c*d
 
-
-def multiplication(n,c,g):
-    print(n*c*g)
+#multiply
+def multiplication(n,c,g,u):
+    print(n*c*g*u)
 
 
 print("bye")
